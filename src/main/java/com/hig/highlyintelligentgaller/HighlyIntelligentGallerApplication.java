7f8cd70ef7c95492327ec6a833d2ae7efe950da0
Niruksorp <prge200000@mail.ru>
2021-01-21 00:02:33 +0700

    init commit

---
+++ src/main/java/com/hig/highlyintelligentgaller/HighlyIntelligentGallerApplication.java
@@ -0,0 +1,13 @@
+package com.hig.highlyintelligentgaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlyIntelligentGallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighlyIntelligentGallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hig/highlyintelligentgaller/HighlyIntelligentGallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hig.highlyintelligentgaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighlyIntelligentGallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
